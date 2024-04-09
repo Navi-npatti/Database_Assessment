f306033ea6bace10a6e312821974720c77b7109e
Nick Patti <69326552+Navi-npatti@users.noreply.github.com>
2024-04-09 13:16:31 -0400

    Initial commit
    Version 8

---
+++ functions/OutputCompileSummary.ps1
@@ -0,0 +1,14 @@
+﻿function OutputCompileSummary {
+
+############ Display output information ################
+Write-Host "" 
+Write-Host "Be sure to review the CompileSuccess.txt and CompileFailures.txt to see if any folders need it reran."
+Write-Host ""
+Write-Host "Make sure the files were correctly converted."
+Write-Host ""
+Write-Host -Fore Green "Work Complete ..."
+Start-Sleep -s 15
+########################################################
+
+
+}
